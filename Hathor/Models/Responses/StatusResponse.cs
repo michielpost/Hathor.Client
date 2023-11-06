505ad4edfeb86fd77d8d779fa451c75b392a1c65
Michiel Post <michiel@michielpost.nl>
2023-11-06 15:02:00 +0100

    added new ServerInfo

--- Hathor/Models/Responses/StatusResponse.cs
+++ Hathor/Models/Responses/StatusResponse.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Hathor.Models.Responses
@@ -10,7 +11,7 @@ namespace Hathor.Models.Responses
     public class StatusResponse : DefaultResponse
     {
         [JsonProperty(PropertyName = "serverInfo")]
-        public string? ServerInfo { get; set; }
+        public ServerInfo? ServerInfo { get; set; }
 
         [JsonProperty(PropertyName = "statusCode")]
         public int? StatusCode { get; set; }
@@ -23,5 +24,38 @@ namespace Hathor.Models.Responses
 
         [JsonProperty(PropertyName = "state")]
         public int? State { get; set; }
+    }
+
+    public class ServerInfo
+    {
+        [JsonProperty("version")]
+        public string? Version { get; set; }
+
+        [JsonProperty("network")]
+        public string? Network { get; set; }
+
+        [JsonProperty("min_weight")]
+        public int MinWeight { get; set; }
+
+        [JsonProperty("min_tx_weight")]
+        public int MinTxWeight { get; set; }
+
+        [JsonProperty("min_tx_weight_coefficient")]
+        public double MinTxWeightCoefficient { get; set; }
+
+        [JsonProperty("min_tx_weight_k")]
+        public int MinTxWeightK { get; set; }
+
+        [JsonProperty("token_deposit_percentage")]
+        public double TokenDepositPercentage { get; set; }
+
+        [JsonProperty("reward_spend_min_blocks")]
+        public int RewardSpendMinBlocks { get; set; }
+
+        [JsonProperty("max_number_inputs")]
+        public int MaxNumberInputs { get; set; }
+
+        [JsonProperty("max_number_outputs")]
+        public int MaxNumberOutputs { get; set; }
     }
 }
